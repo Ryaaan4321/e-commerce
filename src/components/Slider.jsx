1a65871d42b76ab519d589cafabce5d7af6bef66
Ryaaan4321 <aryan056400@gmail.com>
2023-12-10 11:25:52 +0530

    created categories items

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,75 +1,113 @@
 import styled from "styled-components";
-import { ArrowLeftRounded  , ArrowRightRounded} from "@material-ui/icons";
+import { ArrowLeftRounded, ArrowRightRounded } from "@material-ui/icons";
+import { useState } from "react";
+import { sliderItems } from "../data.js";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  position:relative;
+  position:relative
+  overflow:hidden;
+
 `;
 
-const Arrow=styled.div`
-width:50px;
-height:50px;
-background-color:#fff7f7;
-border-radius:50%;
-display:flex;
-align-items:center;
-justify-content:center;
-position:absolute;
-top:0;
-bottom:0;
-left:${props=> props.direction==="left" && "10px"};
-right:${props=> props.direction==="right" && "10px"};
-margin:auto;
-cursor:pointer;
-opacity:0.5;
-`
-
-
-const Wrapper=styled.div`
-height:100`
-
-const Slide=styled.div`
- width:100vw;
- height:100vh
- display:flex
- align-items:center`;
-const ImageContainer=styled.div`
-height:100%;
-flex:1;`;
-const Image=styled.img`
-height;80%;
-`
-const InfoContainer=styled.div`
-flex:1;
-padding:50px
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
 `;
-const Title=styled.h1`
-`
-const Descreption=styled.p``;
-const Button=styled.button``
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition:all 1.5s ease;
+  transform: translateX(${props=>props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+const ImageContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Description = styled.p`
+margin:50px 0px
+font-size:20px
+font-weight:500;
+letter-spacing:3px
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
 const Slider = () => {
+  const [slideIndex, setslideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if(direction==="left"){
+      setslideIndex(slideIndex>0 ? slideIndex-1 : 2)
+    }else {
+      setslideIndex(slideIndex<2 ? slideIndex+1 : 0);
+    }
+  };
+
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftRounded />
       </Arrow>
-      <Wrapper>
-        <Slide>
-        <ImageContainer>
-          <Image src="https://i.ibb.co/XsdmR2c/1.png"/>
-        </ImageContainer>
-        <InfoContainer>
-          <Title/>
-          <Descreption/>
-          <Button/>
-        </InfoContainer>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item)=>(  
+        <Slide  backgroundColor={item.backgroundColor}>
+          
+          <ImageContainer>
+            <Image src={item.img}/>
+          </ImageContainer>
+          <InfoContainer>
+            <Title>{item.title}</Title>
+            <Description>
+             {item.desc}
+            </Description>
+            <Button>Shop Now</Button>
+          </InfoContainer>
         </Slide>
+         ))}
 
       </Wrapper>
-      <Arrow direction="right">
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightRounded />
       </Arrow>
     </Container>
@@ -77,20 +115,3 @@ const Slider = () => {
 };
 
 export default Slider;
-
-/*echo "# new-app" >> README.md
-  git init
-  git add README.md
-  git commit -m "first commit"
-  git branch -M main
-  git remote add origin https://github.com/Ryaaan4321/new-app.git
-  git push -u origin main
-
-
-…or push an existing repository from the command line
-
-  git remote add origin https://github.com/Ryaaan4321/new-app.git
-  git branch -M main
-  git push -u origin main
-
-*/
